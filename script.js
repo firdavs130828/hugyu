8e1708fa18e1e883087e73d50ec04cd11004eabf
firdavs130828 <arabovfirdavs7@gmail.com>
2025-08-04 12:23:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let randomSon=Math.floor(Math.random()*90+10);
+alert("Sizda hozir random son chiqishni boshlaydi: 😎");
+alert("random orqali chiqqan son:" +randomSon);
+confirm("Biz buni 2 ga ko'paytirib bo'lmoqchimiz.");
+alert(`random orqali chiqqan son ${randomSon} \n Bu sonni 2ga ko'paytirdik: ${randomSon*2} \n Bu sonni 2ga bo'ldik: ${randomSon/2} \n Bu sonni 3ga qoldiqli bo'ldik: ${randomSon%3}`);
